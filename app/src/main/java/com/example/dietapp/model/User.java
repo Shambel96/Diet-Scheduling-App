31c8ad1072bc848b67161acd783e559e5b26e485
Shambel Dechu <shambeldechu@gmail.com>
2026-01-04 14:29:27 +0300

    feat: add user model

--- app/src/main/java/com/example/dietapp/model/User.java
+++ app/src/main/java/com/example/dietapp/model/User.java
@@ -1,4 +1,16 @@
 package com.example.dietapp.model;
 
 public class User {
+
+    public int id;
+    public String username;
+    public String email;
+    public String role;
+
+    public User(int id, String username, String email, String role) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.role = role;
+    }
 }
