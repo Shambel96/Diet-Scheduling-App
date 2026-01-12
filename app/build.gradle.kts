d60f4eaa6ead7cbf743bbeadb8344b51794f7809
Shambel Dechu <shambeldechu@gmail.com>
2026-01-12 20:04:52 +0300

    feat: add Espresso, Mockito, and JUnit dependencies for testing setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,15 +29,28 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    testOptions {
+        unitTests.isReturnDefaultValues = true
+    }
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.ext.junit)
+    implementation(libs.espresso.core)
+
+    // Standard Unit Testing
     testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+
+    // Mockito (Use these specific versions)
+    testImplementation("org.mockito:mockito-core:5.11.0")
+
+    // Instrumented testing (for real devices)
+    // ...to testImplementation
+    testImplementation(libs.ext.junit)
+    testImplementation(libs.espresso.core)
+
 }
